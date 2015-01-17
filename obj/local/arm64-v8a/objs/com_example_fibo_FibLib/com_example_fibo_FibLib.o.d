65fb9a675d36d18e06e88da042ab28e0cefb6971
pjbca <peter.burke.john@gmail.com>
2015-01-17 14:56:46 -0800

    initial commit

---
+++ obj/local/arm64-v8a/objs/com_example_fibo_FibLib/com_example_fibo_FibLib.o.d
@@ -0,0 +1,19 @@
+obj/local/arm64-v8a/objs/com_example_fibo_FibLib/com_example_fibo_FibLib.o: \
+ jni/com_example_fibo_FibLib.c jni/com_example_fibo_FibLib.h \
+ /Applications/ndk/platforms/android-21/arch-arm64/usr/include/jni.h \
+ /Applications/ndk/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h \
+ /Applications/ndk/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h \
+ /Applications/ndk/platforms/android-21/arch-arm64/usr/include/android/api-level.h \
+ /Applications/ndk/platforms/android-21/arch-arm64/usr/include/android/log.h
+
+jni/com_example_fibo_FibLib.h:
+
+/Applications/ndk/platforms/android-21/arch-arm64/usr/include/jni.h:
+
+/Applications/ndk/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h:
+
+/Applications/ndk/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h:
+
+/Applications/ndk/platforms/android-21/arch-arm64/usr/include/android/api-level.h:
+
+/Applications/ndk/platforms/android-21/arch-arm64/usr/include/android/log.h:
---
+++ obj/local/armeabi/objs/com_example_fibo_FibLib/com_example_fibo_FibLib.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs/com_example_fibo_FibLib/com_example_fibo_FibLib.o: \
+ jni/com_example_fibo_FibLib.c jni/com_example_fibo_FibLib.h \
+ /Applications/ndk/platforms/android-21/arch-arm/usr/include/jni.h \
+ /Applications/ndk/platforms/android-21/arch-arm/usr/include/sys/cdefs.h \
+ /Applications/ndk/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Applications/ndk/platforms/android-21/arch-arm/usr/include/android/api-level.h \
+ /Applications/ndk/platforms/android-21/arch-arm/usr/include/android/log.h
+
+jni/com_example_fibo_FibLib.h:
+
+/Applications/ndk/platforms/android-21/arch-arm/usr/include/jni.h:
+
+/Applications/ndk/platforms/android-21/arch-arm/usr/include/sys/cdefs.h:
+
+/Applications/ndk/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Applications/ndk/platforms/android-21/arch-arm/usr/include/android/api-level.h:
+
+/Applications/ndk/platforms/android-21/arch-arm/usr/include/android/log.h:
